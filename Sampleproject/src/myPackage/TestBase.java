e2ed2a1a9f4e5d8686752a233352dff2cba2b6a5
SIDDHANT2511 <110152737+SIDDHANT2511@users.noreply.github.com>
2022-08-12 15:09:07 +0530

    Homepage commit
    I have completed my task please review it

--- Sampleproject/src/myPackage/TestBase.java
+++ Sampleproject/src/myPackage/TestBase.java
@@ -3,7 +3,7 @@ package myPackage;
 public class TestBase {
 
 	public static void main(String[] args) {
-		System.out.println("Common properties");
+		System.out.println("Common Properties");
 
 	}
 
---
+++ Sampleproject/src/myPackage1/HomePage.java
@@ -0,0 +1,10 @@
+package myPackage1;
+
+public class HomePage {
+
+	public static void main(String[] args) {
+		System.out.println("HomePage");
+
+	}
+
+}
